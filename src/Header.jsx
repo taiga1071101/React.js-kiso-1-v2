a724d7161233764ad9c9e3d153a01df7c74b321a
taiga1071101 <taigatukimi@gmail.com>
2025-03-02 20:15:10 +0900

    station2クリア

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import './header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <p className="app-title">React掲示板</p>
+    </header>
+  );
+}
+
+export default Header
---
+++ src/ThreadsList.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import './threadsList.css';
+
+const ThreadsList = () => {
+  const [threads, setThreads] = useState([]);
+  const url = "https://railway.bulletinboard.techtrain.dev/threads?offset=0";
+
+  const listDisplay = () => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setThreads(data);
+    })
+    .catch(error => {
+      console.error('データ取得エラー', error);
+    });
+  }
+
+  useEffect(()=> {
+    listDisplay();
+  }, []);
+
+  return (
+    <>
+      <div className="threads-list">
+        <h1>新着スレッド</h1>
+        <ul>
+          {
+            threads.map(thread => {
+              return ( <li key={thread.id}>{thread.title}</li> );
+            })
+          }
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default ThreadsList
